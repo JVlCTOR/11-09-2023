27d6ef515d66dc7a67a75e87fb7af00b4b73af9f
JVlCTOR <joaovictorresende59@gmail.com>
2023-09-11 21:47:17 -0300

    subindo 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const quantidadeVogal = (nome) => {
 //   let soma = 0;
-  
+
 //   for (const letra of nome) {
 //     if (
 //       letra.toLowerCase() === "a" ||
@@ -48,3 +48,53 @@
 //     }
 // }
 // filtrarObjeto(alunos)
+const vendas = [
+    {
+      id: 1,
+      produto: "Computador",
+      quantidade: 15,
+      valor: 2900,
+    },
+    {
+      id: 2,
+      produto: "Monitor",
+      quantidade: 10,
+      valor: 400,
+    },
+    {
+      id: 3,
+      produto: "Cadeira",
+      quantidade: 5,
+      valor: 799,
+    },
+    {
+      id: 4,
+      produto: "Teclado",
+      quantidade: 19,
+      valor: 130,
+    },
+    {
+      id: 5,
+      produto: "Mouse",
+      quantidade: 30,
+      valor: 70,
+    },
+  ];
+  
+  /**
+   * Função - parâmetro (array de objetos)
+   * Calcular o valor total de vendas realizado de todos os protudos juntos
+   * Calcular o valor total por produto
+   * console.log(Valor de venda mensal: R$ VALOR TOTAL de todos os produtos)
+   * console.log(Valor de venda PRODUTO: R$ VALOR TOTAL por PRODUTO)
+   */
+  function calcVendas(vendas){
+    let total = 0
+    for(const venda of vendas){
+        let soma = venda.quantidade * venda.valor
+        total += soma
+    }
+    console.log(`o total é de todos produtos é R$ :${total}`);
+  }
+  calcVendas(vendas)
+
